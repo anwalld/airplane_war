ff208694f1a0a462528561cc298ee4ae75ad084f
anwalld <d20qw1@163.com>
2025-11-23 15:17:40 +0800

    把PlayerManager.h的new改到.cpp中进行

--- arch/behavior/BulletBehavior.h
+++ arch/behavior/BulletBehavior.h
@@ -4,9 +4,6 @@
 //随机出外观
 int AppMatchEnemyType(bullet* b);
 
-//计算vx vy;
-std::pair<int,int> CalcBulletVelocity(bullet* b);
-
 //根据vx vy调用SystemMove;
 std::pair<int,int> CalcuBulletMove(pair<int,int>v,bullet* b);
 #endif //AIRPLANE_WARS_BULLETBEHAVIOR_H
